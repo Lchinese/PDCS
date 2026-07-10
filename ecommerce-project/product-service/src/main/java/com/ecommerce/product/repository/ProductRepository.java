b21b926c0396cb597594f665d27e2adf9c65126f
Lchinese <155833156+Lchinese@users.noreply.github.com>
2026-07-10 15:13:33 +0800

    初始

--- ecommerce-project/product-service/src/main/java/com/ecommerce/product/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.product.repository;
-
-import com.ecommerce.product.entity.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, Integer> {
-}
--- ecommerce-project/user-service/src/main/java/com/ecommerce/user/dto/UserRequest.java
+++
@@ -1,3 +0,0 @@
-package com.ecommerce.user.dto;
-
-public record UserRequest(String username, String password) {}
